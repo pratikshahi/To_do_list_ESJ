e273960f07235f90d5719e47ee93c379e9c27c6d
Pratik Shahi <shahipr0@gmail.com>
2021-03-21 09:36:01 +0530

    made it display weeks

--- app.js
+++ app.js
@@ -15,12 +15,31 @@ app.get("/", function (req, res) {
     var currentDay = today.getDay();
     var day="";
 
-    if (currentDay === 6 || currentDay === 0) {
-      day="Weekend";
-    }
-    else {
-        day="Weekday";
-    
+    switch (currentDay) {
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        case 0:
+            day = "Sunday";
+            break;
+
+        default:
+            break;
     }
     //EJS use it looks for list file inside views so, have to create views folder and list file before
     // using it //inside {} is passing object to list.ejs file
