0fd43f7c385c55ef3c1f7e16204252c4bf94ebb1
Pratik Shahi <shahipr0@gmail.com>
2021-03-21 14:38:31 +0530

    added different work list

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 let items = ["WakeUp", "Brush your Teeth"];
+let workItems=[];
 //telling app to use EJS
 app.set("view engine", "ejs");
 
@@ -28,18 +29,43 @@ app.get("/", function (req, res) {
 
   //EJS use it looks for list file inside views so, have to create views folder and list file before
   // using it //inside {} is passing object to list.ejs file
-  res.render("list", { dayName: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
+
+
   let item = req.body.addedList;
-  //dont use 2 render cause 1st render will already traverse an give error for sencond obj passde
+  //dont use 2 render cause 1st render will already traverse an give error for sencond obj passed
   //insted use redirect and mention let/array on top ,pas onj at same render using ,
   //items  is arry we created on top cause item could only hold 1 data and would replace the old list
-  items.push(item);
 
-  res.redirect("/");
+
+  if(req.body.list==="WorkList"){
+
+    workItems.push(item);
+    res.redirect("/work");
+
+  }
+  else{
+    items.push(item);
+
+    res.redirect("/");
+  }
+
+
 });
+
+//creating work list
+app.get("/work",function(req,res){
+  res.render("list",{listTitle:"WorkList",newListItems:workItems});
+});
+
+app.post("/work",function(req,res){
+  let item=req.body.addedList;
+  workItems.push(addedList);
+  res.redirect("/");
+})
 
 app.listen(3000, function () {
   console.log("server running on port 3000");
